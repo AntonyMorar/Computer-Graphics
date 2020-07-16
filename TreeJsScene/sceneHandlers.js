88c3d10ef546a4b407145ced9b599219ad522387
antony999k <lv999k@hotmail.com>
2020-07-15 20:43:30 -0500

    :sparkles: Treejs Scene Add and clear

--- TreeJsScene/sceneHandlers.js
+++ TreeJsScene/sceneHandlers.js
@@ -1,52 +1,62 @@
-
 // An integer value, in pixels, indicating the X coordinate at which the mouse pointer was located when the event occurred. 
-let mouseDown = false, pageX = 0;
+let mouseDown = false,
+    pageX = 0;
 
-function rotateScene(deltax, group)
-{
+function rotateScene(deltax, group) {
     group.rotation.y += deltax / 100;
     $("#rotation").html("rotation: 0," + group.rotation.y.toFixed(1) + ",0");
 }
 
-function scaleScene(scale, group)
-{
+function scaleScene(scale, group) {
     group.scale.set(scale, scale, scale);
     $("#scale").html("scale: " + scale);
 }
 
-function onMouseMove(evt, group)
-{
+function onMouseMove(evt, group) {
     if (!mouseDown)
         return;
-    
+
     // The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
     evt.preventDefault();
-    
+
     let deltax = evt.pageX - pageX;
     pageX = evt.pageX;
     rotateScene(deltax, group);
 }
 
-function onMouseDown(evt)
-{
+function onMouseDown(evt) {
     evt.preventDefault();
-    
+
     mouseDown = true;
     pageX = evt.pageX;
 }
 
-function onMouseUp(evt)
-{
+function onMouseUp(evt) {
     evt.preventDefault();
-    
+
     mouseDown = false;
 }
 
-function addMouseHandler(canvas, group)
-{
-    canvas.addEventListener( 'mousemove', e => onMouseMove(e, group), false);
-    canvas.addEventListener( 'mousedown', e => onMouseDown(e), false );
-    canvas.addEventListener( 'mouseup',  e => onMouseUp(e), false );
+function addMouseHandler(canvas, group) {
+    canvas.addEventListener('mousemove', e => onMouseMove(e, group), false);
+    canvas.addEventListener('mousedown', e => onMouseDown(e), false);
+    canvas.addEventListener('mouseup', e => onMouseUp(e), false);
 
     $("#slider").on("slide", (e, u) => scaleScene(u.value, group));
+}
+
+function btnEvnts() {
+    document.getElementById("add").addEventListener("click", function () {
+        console.log("Add");
+        AddFigure();
+    });
+
+    document.getElementById("satelite").addEventListener("click", function () {
+        console.log("satelite");
+    });
+
+    document.getElementById("clear").addEventListener("click", function () {
+        console.log("clear");
+        clearFigures();
+    });
 }
--- TreeJsScene/threejsscene.js
+++ TreeJsScene/threejsscene.js
@@ -1,66 +1,44 @@
 let renderer = null, 
 scene = null, 
-camera = null,
-cube = null,
-sphere = null,
-cone = null,
-sphereGroup = null;
+camera = null;
+
+let objects = [];
+let groups = [];
 
 let duration = 5000; // ms
 let currentTime = Date.now();
+let posOffset = {x:0,y:0,z:0};
+let objsSize=0.75;
 
-function animate() 
-{
-    let now = Date.now();
-    let deltat = now - currentTime;
-    currentTime = now;
-    let fract = deltat / duration;
-    let angle = Math.PI * 2 * fract;
-
-    // Rotate the cube about its Y axis
-    cube.rotation.y += angle;
-
-    // Rotate the sphere group about its Y axis
-    sphereGroup.rotation.y -= angle / 2;
-    sphere.rotation.x += angle;
-
-    // Rotate the cone about its X axis (tumble forward)
-    cone.rotation.z += angle;
-}
-
-function run() {
-    requestAnimationFrame(function() { run(); });
-    
-    // Render the scene
-    renderer.render( scene, camera );
-
-    // Spin the cube for next frame
-    animate();
-}
 
 function createScene(canvas)
-{    
+{
+    /****************************************************************************
+     * Scene
+     */
     // Create the Three.js renderer and attach it to our canvas
     renderer = new THREE.WebGLRenderer( { canvas: canvas, antialias: true } );
-
     // Set the viewport size
     renderer.setSize(canvas.width, canvas.height);
-    
     // Create a new Three.js scene
     scene = new THREE.Scene();
-
     // Set the background color 
-    scene.background = new THREE.Color( 0.2, 0.2, 0.2 );
+    scene.background = new THREE.Color( 0.16, 0.16, 0.185 );
     // scene.background = new THREE.Color( "rgb(100, 100, 100)" );
 
+    /****************************************************************************
+     * Camera
+     */
     // Add  a camera so we can view the scene
-    camera = new THREE.PerspectiveCamera( 45, canvas.width / canvas.height, 1, 4000 );
-    camera.position.z = 10;
+    //PerspectiveCamera( fov : Number, aspect : Number, near : Number, far : Number )
+    camera = new THREE.PerspectiveCamera( 45, canvas.width / canvas.height, 0.5, 4000 );
+    camera.position.set(0,3.75,10);
+    camera.rotation.x=-0.35;
     scene.add(camera);
 
-    // Create a group to hold all the objects
-    let cubeGroup = new THREE.Object3D;
-    
+    /****************************************************************************
+     * Light
+     */
     // Add a directional light to show off the objects
     let light = new THREE.DirectionalLight( 0xffffff, 1.0);
     // let light = new THREE.DirectionalLight( "rgb(255, 255, 100)", 1.5);
@@ -75,56 +53,119 @@ function createScene(canvas)
     let ambientLight = new THREE.AmbientLight(0xffccaa, 0.2);
     scene.add(ambientLight);
 
-    let textureUrl = "../images/ash_uvgrid01.jpg";
+    /****************************************************************************
+     * Textures and materials
+     */
+    let textureUrl = "../images/checker_large.gif";
     let texture = new THREE.TextureLoader().load(textureUrl);
     let material = new THREE.MeshPhongMaterial({ map: texture });
 
-    // Create the cube geometry
-    let geometry = new THREE.CubeGeometry(2, 2, 2);
+    /****************************************************************************
+     * Geometry
+     */
 
-    // And put the geometry and material together into a mesh
-    cube = new THREE.Mesh(geometry, material);
+    //Create the first group (empty)
+    AddGroup();
 
-    // Tilt the mesh toward the viewer
-    cube.rotation.x = Math.PI / 5;
-    cube.rotation.y = Math.PI / 5;
+    // Create new Geometry
+    AddFigure(material);
+    AddFigure(material);
 
-    // Add the cube mesh to our group
-    cubeGroup.add( cube );
+    // Add to scene
+    scene.add( groups[0] );
 
-    cubeGroup.position.set(1, 0, -0.5);
-
-    // Create a group for the sphere
-    sphereGroup = new THREE.Object3D;
-    cubeGroup.add(sphereGroup);
-    
-    // Move the sphere group up and back from the cube
-    sphereGroup.position.set(0, 3, -4);
-
-    // Create the sphere geometry
-    geometry = new THREE.SphereGeometry(1, 20, 20);
-    
-    // And put the geometry and material together into a mesh
-    sphere = new THREE.Mesh(geometry, material);
-
-    // Add the sphere mesh to our group
-    sphereGroup.add( sphere );
-
-    // Create the cone geometry
-    geometry = new THREE.CylinderGeometry(0, .333, .444, 20, 5);
-
-    // And put the geometry and material together into a mesh
-    cone = new THREE.Mesh(geometry, material);
-
-    // Move the cone up and out from the sphere
-    cone.position.set(1, 1, -.667);
-        
-    // Add the cone mesh to our group
-    sphereGroup.add( cone );
-    
-    // Now add the group to our scene
-    scene.add( cubeGroup );
-
+    /****************************************************************************
+     * Events
+     */
     // add mouse handling so we can rotate the scene
-    addMouseHandler(canvas, cubeGroup);
+    addMouseHandler(canvas, groups[0]);
+    btnEvnts();
 }
+
+// Add figure to the actual group
+function AddFigure(material=null){
+    if(groups.length==0 || groups == null) AddGroup();
+    // Create new Geometry
+    let r = Math.floor(Math.random() * 4);
+    let newObj;
+    if(material == null) material = new THREE.MeshPhongMaterial( {color: 0xffff00} );
+    switch(r){
+        case(0):
+            newObj = new THREE.DodecahedronGeometry(objsSize,0);
+            break;
+        case(1):
+            newObj = new THREE.IcosahedronGeometry(objsSize,0);   
+            break;
+        case(2):
+            newObj = new THREE.OctahedronGeometry(objsSize,0);
+            break;
+        case(3):
+            newObj = new THREE.SphereGeometry( objsSize, 12, 8 );
+            break;
+        default:
+            newObj = new THREE.BoxGeometry( objsSize, 1, 1 );
+            break;
+    }
+    let newMesh = new THREE.Mesh(newObj, material);
+    newMesh.position.x += posOffset.x;
+    newMesh.position.y += posOffset.y;
+    newMesh.position.z += posOffset.z;
+    objects.push(newMesh);
+
+    //Change init params
+    rSign = Math.random() >= 0.5; 
+    randomX = (Math.random() * 1.9) + 0.9;
+    randomZ = (Math.random() * 1.9) + 0.9;
+    posOffset.x += randomX;
+    posOffset.y = (Math.random()*2)-1;
+    posOffset.z += randomZ;
+    if(objects.length == 1) objsSize=0.5;
+
+    // Add to the group
+    groups[groups.length - 1].add(objects[objects.length -1]);
+}
+
+function AddGroup(){
+    console.log("creando")
+    // Create a group to hold all the objects
+    let newGroup = new THREE.Object3D;
+    newGroup.position.set(0, 0, 0);
+    groups.push(newGroup);
+}
+
+function clearFigures(){
+    while(objects.length > 0){
+        groups[groups.length - 1].remove(objects.pop());
+    }
+    posOffset = {x:0,y:0,z:0}
+}
+
+function animate() 
+{
+    let now = Date.now();
+    let deltat = now - currentTime;
+    currentTime = now;
+    let fract = deltat / duration;
+    let angle = Math.PI * 2 * fract;
+
+    // Base revolution about its Y axis
+    groups.forEach(gp => {
+        gp.rotation.y -= angle / 2;
+    });
+
+    /*
+    // Base rotation about its Y axis
+    objects.forEach(object => {
+        object.rotation.y += angle;
+    });
+    */
+}
+
+function run() {
+    requestAnimationFrame(function() { run(); });
+    // Render the scene
+    renderer.render( scene, camera );
+
+    animate();
+}
+
