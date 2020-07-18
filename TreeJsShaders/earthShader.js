17aa5c8410c001d7db3f46b56f63e69e2cc6329d
antony999k <lv999k@hotmail.com>
2020-07-17 20:11:57 -0500

    :sparkles: Sun shader

---
+++ TreeJsShaders/earthShader.js
@@ -0,0 +1,104 @@
+var container;
+
+var camera, scene, renderer, composer, clock;
+
+var uniforms, mesh;
+
+init();
+animate();
+
+function init(canvas) {
+
+	camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 3000);
+	camera.position.z = 4;
+
+	scene = new THREE.Scene();
+
+	clock = new THREE.Clock();
+
+	var textureLoader = new THREE.TextureLoader();
+
+	uniforms = {
+
+		"fogDensity": {
+			value: 0.45
+		},
+		"fogColor": {
+			value: new THREE.Vector3(0, 0, 0)
+		},
+		"time": {
+			value: 1.0
+		},
+		"uvScale": {
+			value: new THREE.Vector2(1.0, 1.0)
+		},
+		"baseTexture": {
+			value: textureLoader.load('../images/planets/earthmap1k.jpg')
+		},
+		"bumpTexture": {
+			value: textureLoader.load('../images/planets/earthbump1k.jpg')
+		},
+		"cloudsTexture": {
+			value: textureLoader.load('../images/planets/earthcloudmap.jpg')
+		},
+		"cloudsAlphaTexture": {
+			value: textureLoader.load('../images/planets/earthcloudmaptrans.jpg')
+		},
+	};
+
+	uniforms["baseTexture"].value.wrapS = uniforms["baseTexture"].value.wrapT = THREE.RepeatWrapping;
+	uniforms["bumpTexture"].value.wrapS = uniforms["bumpTexture"].value.wrapT = THREE.RepeatWrapping;
+	uniforms["cloudsTexture"].value.wrapS = uniforms["cloudsTexture"].value.wrapT = THREE.RepeatWrapping;
+	uniforms["cloudsAlphaTexture"].value.wrapS = uniforms["cloudsAlphaTexture"].value.wrapT = THREE.RepeatWrapping;
+
+	var size = 0.65;
+
+	var material = new THREE.ShaderMaterial({
+		uniforms: uniforms,
+		vertexShader: document.getElementById('vertexShader').textContent,
+		fragmentShader: document.getElementById('fragmentShader').textContent,
+		transparent: true,
+	});
+
+	mesh = new THREE.Mesh(new THREE.SphereGeometry(size, 30, 30), material);
+	mesh.rotation.x = 0.3;
+	scene.add(mesh);
+
+	renderer = new THREE.WebGLRenderer({
+		canvas: canvas,
+		antialias: true
+	});
+	renderer.setPixelRatio(window.devicePixelRatio);
+
+	onWindowResize();
+
+	window.addEventListener('resize', onWindowResize, false);
+
+}
+
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+//
+
+function animate() {
+
+	requestAnimationFrame(animate);
+	render();
+
+}
+
+function render() {
+
+	var delta = 5 * clock.getDelta();
+
+	uniforms["time"].value += 0.2 * delta;
+
+	renderer.render(scene, camera);
+}
